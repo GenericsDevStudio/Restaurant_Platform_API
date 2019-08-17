a79ee6dcecf4294c3f76f1c6761a3f23624dbd8b
Oleg Fomenko <oleg.fomenko2002@yandex.ru>
2019-08-17 22:34:29 +0300

    bugfix

--- src/main/java/com/generics/restaurant_platform/api/rest_controllers/order/OrderAddingController.java
+++ src/main/java/com/generics/restaurant_platform/api/rest_controllers/order/OrderAddingController.java
@@ -4,14 +4,10 @@ import com.generics.restaurant_platform.api.database.order.OrderDataBaseControll
 import com.generics.restaurant_platform.api.entities.Order;
 import com.generics.restaurant_platform.api.json_answers.Answer;
 import com.generics.restaurant_platform.api.json_answers.RegistrationAnswer;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 public class OrderAddingController {
